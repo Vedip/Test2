90bdeb5b788e1feef687a45d2124d438cbe3dbf4
Vendip <vendipn@gmail.com>
2020-04-22 20:06:20 +0100

    This is my second assignment

---
+++ Test/Test/Details.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vendip
+ * Date: 4/22/2020
+ * Time: 12:00 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Test
+{
+	/// <summary>
+	/// Description of Details.
+	/// </summary>
+	public class Details
+	{
+		public string FirstName;
+		public string LastName;
+	}
+}
---
+++ Test/Test/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vendip
+ * Date: 4/22/2020
+ * Time: 8:21 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Test
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Details EmployeeDetails = new Details();
+			EmployeeDetails.FirstName = "Vendip ";
+			EmployeeDetails.LastName = "Nanpyal";
+			Console.Write(EmployeeDetails.FirstName);
+			Console.WriteLine(EmployeeDetails.LastName);
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
